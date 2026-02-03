28dd69682c4cad56da5a5236d63cd795a45d3b2c
Abhishek Choudhary <abhich2109@gmail.com>
2026-02-03 14:39:54 +0530

    feat: updated the port

--- backend/server.js
+++ backend/server.js
@@ -81,7 +81,7 @@ app.use((err, req, res, next) => {
     res.status(500).json({ success: false, message: 'Server Error' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
